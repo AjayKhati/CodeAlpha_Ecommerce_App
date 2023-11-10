0e1063950ff34a9dbcfbb54fc2a946fc34fbd452
Ajay Khati <ajaysingh0001123@gmail.com>
2023-11-11 00:02:50 +0530

    in Development

---
+++ app/src/main/java/com/ajkhati/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.ajkhati.ecommerce.Prevalent;
+
+import com.ajkhati.ecommerce.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
